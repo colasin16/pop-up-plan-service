48d352d4f50a0368aa354df5d7617b3093ab677c
Hessam Ghassemi <44605128+hqasemi@users.noreply.github.com>
2022-03-12 13:10:22 +0330

    implement findById API (#4)

---
+++ src/planner/views/findPlanByIdView.ts
@@ -0,0 +1,22 @@
+import { PlanRepository } from "../models/PlanRepository";
+import { Identifier } from "../models/Identifier";
+import { Plan } from "../models/Plan";
+import { ObjectId } from "bson";
+
+export interface FindPlanByIdMessage {
+  id: string;
+}
+
+export class FindPlanByIdView {
+  private planRepository: PlanRepository;
+
+  constructor(planRepository: PlanRepository) {
+    this.planRepository = planRepository;
+  }
+
+  public interact(message: FindPlanByIdMessage): Plan | null {
+    const plan_id =  new Identifier(new ObjectId(message.id));
+    const plan = this.planRepository.find(plan_id);
+    return plan;
+  }
+}
