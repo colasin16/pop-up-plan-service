91ea2dccea021b75e3d90a308e19a7346dc5ae7c
Hessam Ghassemi <44605128+hqasemi@users.noreply.github.com>
2022-05-03 13:58:54 +0430

    Update src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts

--- src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts
+++ src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts
@@ -23,6 +23,8 @@ export function authenticateTokenMiddleware(req, res, next) {
 
         if (err) return res.sendStatus(403)
 
+        // here we hold userId in `req` object, so in all the views we can find who sends the request.
+        // TODO: add role later, maybe for some APIs we need to limit the access
         req.userId = payload.userId
 
         next()
