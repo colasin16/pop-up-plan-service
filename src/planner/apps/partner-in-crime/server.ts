d1c8f159d7b222d106e20221369752498edf068b
Hessam <s.hesam.ghasemi@gmail.com>
2022-05-03 12:30:16 +0430

    Define a constant for LOGIN API URL

--- src/planner/apps/partner-in-crime/server.ts
+++ src/planner/apps/partner-in-crime/server.ts
@@ -5,6 +5,7 @@ import * as http from "http";
 import cors from "cors";
 import { registerRoutes } from "./routes";
 import { authenticateTokenMiddleware } from "./middlewares/VerifyToken";
+import { USER_PATH } from "./routes/user.route";
 
 export class Server {
   private express: express.Express;
@@ -16,8 +17,7 @@ export class Server {
     this.express = express();
     this.express.use(bodyParser.json());
     this.express.use(cors());
-    // TODO: exclude login api: https://thewebdev.info/2021/09/12/how-to-exclude-a-route-from-running-an-express-middleware/
-    this.express.use(this.unless('/login', authenticateTokenMiddleware))
+    this.express.use(this.unless(USER_PATH.LOGIN, authenticateTokenMiddleware))
     const router = Router();
     this.express.use(router);
     registerRoutes(router);
@@ -49,6 +49,14 @@ export class Server {
     });
   }
 
+  /**
+   * 
+   * exclude a specific API to being process by a middleware
+   * ref: https://thewebdev.info/2021/09/12/how-to-exclude-a-route-from-running-an-express-middleware/
+   * @param path 
+   * @param middleware 
+   * @returns 
+   */
   private unless = (path, middleware) => {
     return (req, res, next) => {
       if (path === req.path) {
