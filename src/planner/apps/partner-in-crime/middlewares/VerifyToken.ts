98cec79b4bacd62284ad3874915469c73e59a31c
Hessam Ghassemi <44605128+hqasemi@users.noreply.github.com>
2022-05-04 11:30:23 +0430

    Update src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts

--- src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts
+++ src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts
@@ -19,9 +19,8 @@ export function authenticateTokenMiddleware(req, res, next) {
     jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, payload: JwtTokenPayload) => {
         if (err) {
             console.error(err)
+            return res.sendStatus(403)
         }
-
-        if (err) return res.sendStatus(403)
 
         // here we hold userId in `req` object, so in all the views we can find who sends the request.
         // TODO: add role later, maybe for some APIs we need to limit the access
