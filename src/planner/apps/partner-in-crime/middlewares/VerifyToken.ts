b5568c3695420358960cdd3a982f6bd3af44a12f
Hessam Ghassemi <44605128+hqasemi@users.noreply.github.com>
2022-05-04 11:30:33 +0430

    Update src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts
    Co-authored-by: colasin16 <74024149+colasin16@users.noreply.github.com>

--- src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts
+++ src/planner/apps/partner-in-crime/middlewares/VerifyToken.ts
@@ -1,15 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { JwtTokenPayload } from '../../../types/JwtTokenPayload';
 
-/**
- * This is a middleware in which token is checked in http authorization header, 
- * and it forbids unuthenticated requests
- * reference: https://www.digitalocean.com/community/tutorials/nodejs-jwt-expressjs
- * @param req 
- * @param res 
- * @param next 
- * @returns
- */
 export function authenticateTokenMiddleware(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
